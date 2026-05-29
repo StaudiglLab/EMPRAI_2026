e83654d6486ac741496985ad71ec25db976e3f01
chowdhuryaditya <chowdhury@ncra.tifr.res.in>
2026-05-29 08:58:33 +0200

    tutorial II

---
+++ tutorial-II/5_functions.py
@@ -0,0 +1,6 @@
+
+#TODO:
+#1. put total time on fixation inside a function
+#1. show function calling, order of arguements
+#2. show default arguements vs. mandetory ones
+#3. show how functions can return values
